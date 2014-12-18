c77af0489c062e5cd71de53661db8f3f6447da47
Roemer <r.baeriswyl@gmail.com>
2014-12-18 17:12:57 +0100

    - Added MVVM tools - Added AutoUpdater - Added PortableConfiguration - Added TextFormatter

---
+++ src/FlauLib/Tools/AutoUpdateBase.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace FlauLib.Tools
+{
+    public abstract class AutoUpdateBase
+    {
+        public void CheckAndUpdate()
+        {
+            // Wait until other instances of this process are closed
+            var currentProcess = Process.GetCurrentProcess();
+            var processes = Process.GetProcessesByName(currentProcess.ProcessName);
+            // Loop through processes with the same name 
+            foreach (var process in processes)
+            {
+                // Ignore the current process
+                if (process.Id == currentProcess.Id) continue;
+                // Close or kill other instances
+                process.Close();
+                process.WaitForExit(1000);
+                // Ignore closed processes
+                if (process.HasExited) continue;
+                process.Kill();
+                process.WaitForExit(1000);
+            }
+
+            // Update routine
+            const string updateDirName = "AutoUpdateTemp";
+            string exeName = AppDomain.CurrentDomain.FriendlyName;
+            string currDir = Directory.GetCurrentDirectory();
+            if (currDir.EndsWith(updateDirName))
+            {
+                // We're running in temp dir
+                var mainDir = Directory.GetParent(currDir).FullName;
+                var tempDir = currDir;
+                // Copy to main
+                foreach (var file in new DirectoryInfo(tempDir).GetFiles())
+                {
+                    file.CopyTo(Path.Combine(mainDir, file.Name), true);
+                }
+                // Run from main
+                var startInfo = new ProcessStartInfo(Path.Combine(mainDir, exeName));
+                startInfo.WorkingDirectory = mainDir;
+                Process.Start(startInfo);
+                Terminate();
+            }
+            else
+            {
+                // We're running in main directory
+                var tempDir = Path.Combine(currDir, updateDirName);
+                // Delete possibly existing temp
+                if (Directory.Exists(tempDir)) { Directory.Delete(tempDir, true); }
+
+                // Get versions
+                var currentVersion = GetCurrentVersion();
+                var newestVersion = GetNewestVersion();
+
+                // Compare versions
+                if (currentVersion != newestVersion)
+                {
+                    // Version changed, confirm the update
+                    if (UpdateConfirmation(currentVersion, newestVersion))
+                    {
+                        // Prepare new version
+                        PrepareNewVersion(tempDir);
+                        // Run from temp
+                        var startInfo = new ProcessStartInfo(Path.Combine(tempDir, exeName));
+                        startInfo.WorkingDirectory = tempDir;
+                        Process.Start(startInfo);
+                        Terminate();
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Get the current version
+        /// </summary>
+        /// <returns>Current version-string</returns>
+        public abstract string GetCurrentVersion();
+
+        /// <summary>
+        /// Get the newest version
+        /// </summary>
+        /// <returns>Newest version-string</returns>
+        public abstract string GetNewestVersion();
+
+        /// <summary>
+        /// Terminate the application
+        /// </summary>
+        public abstract void Terminate();
+
+        /// <summary>
+        /// Prepare the new version of the application in the given directory
+        /// </summary>
+        /// <param name="preparationDirectoryPath">The directory where the new version must be placed</param>
+        public abstract void PrepareNewVersion(string preparationDirectoryPath);
+
+        /// <summary>
+        /// Optional method to confirm the update
+        /// </summary>
+        /// <param name="currentVersion">The current version-string</param>
+        /// <param name="newestVersion">The newest version-string</param>
+        /// <returns>true if it should update, false otherwise</returns>
+        public virtual bool UpdateConfirmation(string currentVersion, string newestVersion)
+        {
+            return true;
+        }
+    }
+}
---
+++ src/FlauLib/Tools/TextFormatter.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FlauLib.Tools
+{
+    public static class TextFormatter
+    {
+        /// <summary>
+        /// Converts a List of string arrays to a string where each element in each line is correctly padded.
+        /// Make sure that each array contains the same amount of elements!
+        /// <example>
+        /// --- Without:
+        /// Title Name Street
+        /// Mr. Roman Sesamstreet
+        /// Mrs. Claudia Abbey Road
+        /// --- With:
+        /// Title   Name      Street
+        /// Mr.     Roman     Sesamstreet
+        /// Mrs.    Claudia   Abbey Road
+        /// </example>
+        /// <param name="lines">List lines, where each line is an array of elements for that line.</param>
+        /// <param name="padding">Additional padding between each element (default = 1)</param>
+        /// </summary>
+        public static string PadElementsInLines(List<string[]> lines, int padding = 1)
+        {
+            // Calculate maximum numbers for each element accross all lines
+            var numElements = lines[0].Length;
+            var maxValues = new int[numElements];
+            for (int i = 0; i < numElements; i++)
+            {
+                maxValues[i] = lines.Max(x => (x[i] ?? String.Empty).Length) + padding;
+            }
+
+            var sb = new StringBuilder();
+            // Build the output
+            bool isFirst = true;
+            foreach (var line in lines)
+            {
+                if (!isFirst)
+                {
+                    sb.AppendLine();
+                }
+                isFirst = false;
+
+                for (int i = 0; i < line.Length; i++)
+                {
+                    var value = line[i] ?? String.Empty;
+                    // Append the value with padding of the maximum length of any value for this element
+                    sb.Append(value.PadRight(maxValues[i]));
+                }
+            }
+            return sb.ToString();
+        }
+
+        private static readonly string[] SizeSuffixes = { "bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
+        /// <summary>
+        /// Converts a given number of bytes to a string with the appropriate suffix (eg. KB)
+        /// </summary>
+        /// <example>
+        /// Simply convert some bytes. This will return "2KB"
+        /// <code>
+        /// SizeSuffix(2048);
+        /// </code>
+        /// </example>
+        /// <param name="numBytes">The number of bytes</param>
+        /// <param name="decimalPlaces">The number of decimal places</param>
+        /// <returns>A string with the appropriate suffix</returns>
+        public static string SizeSuffix(long numBytes, int decimalPlaces = 0)
+        {
+            if (numBytes < 0)
+            {
+                throw new ArgumentException("Bytes should not be negative", "numBytes");
+            }
+            var mag = (int)Math.Max(0, Math.Log(numBytes, 1024));
+            var adjustedSize = Math.Round(numBytes / Math.Pow(1024, mag), decimalPlaces);
+            return String.Format("{0} {1}", adjustedSize, SizeSuffixes[mag]);
+        }
+    }
+}
