200bb2a36d71e13fbf0a6ce46be4ffae2c7b9bca
Roemer <r.baeriswyl@gmail.com>
2014-12-19 11:46:46 +0100

    - Added missing unittest files

---
+++ src/FlauLib.Tests/TextFormatterTestFixture.cs
@@ -0,0 +1,23 @@
+﻿using FlauLib.Tools;
+using NUnit.Framework;
+
+namespace FlauLib.Tests
+{
+    [TestFixture]
+    public class TextFormatterTestFixture
+    {
+        [Test]
+        public void SizeSuffixTest()
+        {
+            Assert.AreEqual("1000 bytes", TextFormatter.SizeSuffix(1000));
+            Assert.AreEqual("2 KB", TextFormatter.SizeSuffix(2048));
+            Assert.AreEqual("977 KB", TextFormatter.SizeSuffix(1000000));
+        }
+
+        [Test]
+        public void FailingTest()
+        {
+            Assert.AreEqual(true, false);
+        }
+    }
+}
