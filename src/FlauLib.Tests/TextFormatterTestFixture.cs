f2688b7a0c93e7884385eba32cca9b0d2b8b8cad
Roemer <r.baeriswyl@gmail.com>
2014-12-19 11:50:22 +0100

    - Removed failing test

--- src/FlauLib.Tests/TextFormatterTestFixture.cs
+++ src/FlauLib.Tests/TextFormatterTestFixture.cs
@@ -13,11 +13,5 @@ namespace FlauLib.Tests
             Assert.AreEqual("2 KB", TextFormatter.SizeSuffix(2048));
             Assert.AreEqual("977 KB", TextFormatter.SizeSuffix(1000000));
         }
-
-        [Test]
-        public void FailingTest()
-        {
-            Assert.AreEqual(true, false);
-        }
     }
 }
