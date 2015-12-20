1580830d84bc5f6005ce0f0bf913eeca90cfd5ee
Roemer <r.baeriswyl@gmail.com>
2015-12-20 20:48:00 +0100

    Added test with xUnit

--- src/FlauLib.Tests/TextFormatterTests.cs
+++ src/FlauLib.Tests/TextFormatterTests.cs
@@ -1,5 +1,7 @@
 ﻿using FlauLib.Tools;
 using NUnit.Framework;
+using Xunit;
+using Assert = NUnit.Framework.Assert;
 
 namespace FlauLib.Tests
 {
@@ -12,6 +14,15 @@ namespace FlauLib.Tests
             Assert.AreEqual("1000 bytes", TextFormatter.SizeSuffix(1000));
             Assert.AreEqual("2 KB", TextFormatter.SizeSuffix(2048));
             Assert.AreEqual("977 KB", TextFormatter.SizeSuffix(1000000));
+        }
+
+        
+        [Fact]
+        public void SizeSuffixTest2()
+        {
+            Xunit.Assert.Equal("1000 bytes", TextFormatter.SizeSuffix(1000));
+            Xunit.Assert.Equal("2 KB", TextFormatter.SizeSuffix(2048));
+            Xunit.Assert.Equal("977 KB", TextFormatter.SizeSuffix(1000000));
         }
     }
 }
